92352c6a7fe64ea77a0e843620fa5b47a91777b8
yui-0224 <144981005+yui-0224@users.noreply.github.com>
2023-09-21 23:55:50 +0000

    r

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,60 @@
 import ephem
+orbit_data = "test, E, 1/10.1/2017, 40.8, 158.3, 0.075, 270.5, 347.7, 1.002, 0.000002, 2543"
+satellite = ephem.readdb(orbit_data)
+satellite.compute(2017/01/15 01:00:00)
+latitude = satellite.sublat / ephem.degree
+longitude = satellite.sublong / ephem.degree
+print(latitude, longitude)
+# coding: utf-8
+ 
+# 必要なモジュールのインポート
+import numpy as np
+import ephem
+import time
 import matplotlib.pyplot as plt
-import os
+import matplotlib.cm as cm
+from mpl_toolkits.basemap import Basemap
 from datetime import datetime as dt
 from datetime import timedelta as td
-from matplotlib.animation import ArtistAnimation #アニメーション作成用
-from mpl_toolkits.basemap import Basemap #世界地図描画用
-line1 = 'QZS-4 (MICHIBIKI-4)'
-line2 = '1 42965U 17062A   22057.72757807 -.00000328  00000-0  00000-0 0  9995'
-line3 = '2 42965  40.7914   3.5343 0749421 269.5117 284.1873  1.00283299 16058'
-epoch_date = "22057.72757807" 
+ 
+# 人工衛星の軌道要素を入力（書き下し形式）
+object_name = "Michibiki"       # 衛星名
+epoch_date = "17235.85543054"   # 以下の軌道要素が観測された時刻
+inclination = 40.8777           # 軌道平面の傾き（度）
+RAofAN = 158.3480               # 昇交点の赤経（度, 春分点基準）
+eccentricity = 0.0754024        # 軌道の離心率 (必ず1未満)  　　
+arg_perigee = 270.5910          # 近点引数（度）
+mean_anomaly = 347.7789         # 平均近点角(度)
+mean_motion = 1.00258630        # 平均回転数（周回/日)
+decay_rate = 0.00000228         # 軌道減衰率（周回/日^2)
+orbit_num = 2543                # これまでの積算周回数
+ 
+# epoch_dateをXEphem用の形式に変換
 year = int(epoch_date[:2])
-year += 2000
-datetime = float(epoch_date[2:])
-decimal_time = dt(year, 1, 1) + td(datetime - 1)
+if (year >= 57):
+    year += 1900
+else:
+    year += 2000
+dayno = float(epoch_date[2:])
+epoch_date_sl = "1/%s/%s" % (dayno, year)
+ 
+# epoch_dateから一分ごとの時刻を3600個作成
+decimal_time = dt(year, 1, 1) + td(dayno - 1)
 time_list = [(decimal_time + td(hours=i/60)).strftime("%Y/%m/%d %H:%M:%S") for i in range(0,3600)]
-images = [] #画像リスト用
-fig = plt.figure() #描画領域を作成
-
-m = Basemap() #世界地図を表示
-m.drawcoastlines() #海岸線を表示
-
-for time in time_list:
-    image = []
-    satellite = ephem.readtle(line1, line2, line3) #TLEパラメータを読み込む
-    satellite.compute(time) #指定時刻で実行
-    latitude = satellite.sublat / ephem.degree #緯度を取得
-    longitude = satellite.sublong / ephem.degree #経度を取得
-    x,y = m(longitude, latitude) #XY座標を取得
-    image += m.plot(x, y, 'bo', markersize=7) #青丸でプロット
-    images.append(image) #画像リストに追加
-
-save_dir = "./" #動画の格納先
-ani = ArtistAnimation(fig, images, interval=10, repeat=False) #10ミリ秒毎に図を切り替える
-ani.save(os.path.join(save_dir,'orbit.mp4')) #動画を格納
+ 
+# 世界地図を描写
+m = Basemap()
+m.drawcoastlines()
+ 
+# 一分ごとの人工衛星の位置をプロット
+for item in time_list:
+    orbit_data = ",".join(map(str, (object_name, "E", epoch_date_sl, inclination, RAofAN, eccentricity, arg_perigee, mean_anomaly, mean_motion, decay_rate, orbit_num)))
+    satellite = ephem.readdb(orbit_data)
+    satellite.compute('%s' % item)
+    latitude = satellite.sublat / ephem.degree
+    longitude = satellite.sublong / ephem.degree
+    x1,y1 = m(longitude, latitude)
+    m.plot(x1, y1, "m.", markersize=1)
+ 
+# プロット表示
+plt.show()
